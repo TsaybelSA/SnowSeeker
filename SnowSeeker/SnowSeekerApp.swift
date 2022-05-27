967b0c7da4f062e989887100582c5917a568da28
TsaybelSA <tsaybelsa@gmail.com>
2022-05-27 09:11:59 +0600

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Сергей Цайбель on 27.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
